78f559a362e824f923475f6da98f809525e9fcb5
jon borchardt <jon.borchardt@gmail.com>
2019-06-27 15:25:54 -0700

    theme script fix

--- ui/scripts/buildOverrides.sh
+++ ui/scripts/buildOverrides.sh
@@ -5,9 +5,17 @@ yarn less-watch-compiler ./src/lib/theme ./public varnish-theme.less --enable-js
 # cleanup
 paths=(
     "./public/theme-overrides.less"
-    "./src/lib/theme/*.js"
-    "./src/lib/components/*.js"
-    "./src/lib/utils/*.js"
+    "./src/lib/theme/breakpoints.js"
+    "./src/lib/theme/button.js"
+    "./src/lib/theme/colors.js"
+    "./src/lib/theme/DefaultVarnishTheme.js"
+    "./src/lib/theme/fontWeight.js"
+    "./src/lib/theme/generateOverrides.js"
+    "./src/lib/theme/link.js"
+    "./src/lib/theme/palette.js"
+    "./src/lib/theme/spacing.js"
+    "./src/lib/theme/typography.js"
+    "./src/lib/utils/base.js"
 )
 for path in "${paths[@]}"
 do
